f81450f5b404e5f4d43a9480c2e6dd3951100bb3
Mahmoud Shaheen <mxshaheen@outlook.com>
2020-04-02 06:23:12 +0200

    add digital signature service

---
+++ src/Web/Infra/Installer.cs
@@ -0,0 +1,17 @@
+﻿using EGID.Web.Infra.Crypto;
+using EGID.Web.Infra.DigitalSignature;
+using EGID.Web.Infra.KeysGenerator;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EGID.Web.Infra
+{
+    public static class Installer
+    {
+        public static void AddServices(IServiceCollection services)
+        {
+            services.AddTransient<IKeyGeneratorService, KeyGeneratorService>();
+            services.AddTransient<IAesCryptoService, AesCryptoService>();
+            services.AddTransient<IDigitalSignatureService, DigitalSignatureService>();
+        }
+    }
+}
