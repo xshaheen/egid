c305b1f86ec33a9a875f9c31b666b6f7da64d99b
Mahmoud Shaheen <mxshaheen@outlook.com>
2020-04-05 13:31:00 +0200

    FE | add Cairo font

--- src/Web/ClientApp/src/app/views/admin/employees/employees.component.ts
+++ src/Web/ClientApp/src/app/views/admin/employees/employees.component.ts
@@ -1,7 +1,17 @@
 import { Component } from "@angular/core";
+import { DataSource } from "src/app/services/data-source.service";
+import { ICitizen } from "src/app/models/citizen.model";
 
 @Component({
   templateUrl: "./employees.component.html",
   styleUrls: ["./employees.component.scss"],
 })
-export class EmployeesComponent {}
+export class EmployeesComponent {
+  constructor(private data: DataSource) {}
+
+  getEmployees(): ICitizen[] {
+    const employees: ICitizen[] = [];
+    this.data.getEmployees().subscribe((e) => employees.push(e));
+    return employees;
+  }
+}
