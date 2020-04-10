c5239e7f0bfafd184fe4f027e434d1a51ac0d186
Mahmoud Shaheen <mxshaheen@outlook.com>
2020-04-10 09:46:55 +0200

    BE | organize projects

---
+++ src/Core/Common/ErrorHandler/ErrorMessagesEnum.cs
@@ -0,0 +1,17 @@
+﻿namespace EGID.Core.Common.ErrorHandler
+{
+    public enum ErrorMessages
+    {
+        EntityNull = 1,
+        EntityNotFounded = 2,
+        ModelValidation = 3,
+        AuthUserDoesNotExists = 4,
+        AuthWrongCredentials = 5,
+        AuthCannotCreate = 6,
+        AuthCannotDelete = 7,
+        AuthCannotUpdate = 8,
+        AuthNotValidInformation = 9,
+        AuthCannotRetrieveToken = 10,
+        DbCannotSaveChanges = 11
+    }
+}
---
+++ src/Data/DataSeed.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EGID.Data
+{
+    public class DataSeed
+    {
+        public static void CreateRoles(IServiceProvider services)
+        {
+            var roleManager = services.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
+
+            if (roleManager.Roles.Any()) return;
+
+            var roles = Roles.GetRoles();
+        }
+
+        public static void Admin(IServiceProvider services, IConfiguration config)
+        {
+            var username = config.GetSection("AdministratorIdentity")["username"];
+            var password = config.GetSection("AdministratorIdentity")["password"];
+        }
+    }
+}
