b45c5317a741d865d0d0f7a41f077ab8cc1f793f
Mahmoud Shaheen <mxshaheen@outlook.com>
2020-04-25 19:16:21 +0200

    BE | Test Authentication

--- src/Web/Controllers/AuthController.cs
+++ src/Web/Controllers/AuthController.cs
@@ -1,7 +1,16 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
 using EGID.Data.Cards;
-using EGID.Data.Cards.Dto;
+using EGID.Infrastructure.Auth;
+using EGID.Infrastructure.KeysGenerator;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace EGID.Web.Controllers
 {
@@ -9,21 +18,72 @@ namespace EGID.Web.Controllers
     [Route("[controller]/[action]")]
     public class AuthController : ControllerBase
     {
-        private readonly ICardService _service;
+        private readonly IKeyGeneratorService _keyGenerator;
+        private readonly IConfiguration _configuration;
 
-        public AuthController(ICardService service) => _service = service;
+        public AuthController(IKeyGeneratorService keyGenerator, IConfiguration configuration)
+        {
+            _keyGenerator = keyGenerator;
+            _configuration = configuration;
+        }
 
         [HttpPost]
-        public async Task<ActionResult> SignIn([FromBody] SignInDto signInDto)
+        public ActionResult SignIn()
         {
-            if (ModelState.IsValid)
-                return UnprocessableEntity(ModelState.Values);
+            // validate
+            // if (ModelState.IsValid)
+            //     return UnprocessableEntity(ModelState.Values);
 
-            var result = await _service.SignInAsync(signInDto);
-            if (result.Failed)
-                return BadRequest(result.Errors);
+            var card = new Card
+            {
+                Id = Guid.NewGuid().ToString(),
+                PublicKeyXml = _keyGenerator.PublicKeyXml,
+                PrivateKeyXml = _keyGenerator.PrivateKeyXml,
+                Puk = "123456",
+                Pin1 = "123456",
+                Pin2 = "123456",
+            };
 
-            return Ok(result.Data);
+            // process
+
+            // if (result.Failed)
+            //     return BadRequest(result.Errors);
+
+            return Ok(GenerateJwtToken(card.Id));
+        }
+
+        private string GenerateJwtToken(string cardId)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, cardId),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            // Credentials
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtKey"]));
+
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+            // Expires
+            var minutes = Convert.ToDouble(_configuration["JwtExpireMinutes"]);
+            var expires = DateTime.Now.AddMinutes(minutes);
+
+            var token = new JwtSecurityToken(
+                _configuration["JwtIssuer"],
+                _configuration["JwtIssuer"],
+                claims,
+                expires: expires,
+                signingCredentials: cred
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public string Secret()
+        {
+            return "This is secret";
         }
     }
-}
