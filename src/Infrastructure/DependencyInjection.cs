98e0541e433ba27f54ba4bb207eb57bcda4452af
Mahmoud Shaheen <mxshaheen@outlook.com>
2020-04-25 19:09:10 +0200

    BE | move auth to infrastructure layer

---
+++ src/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using EGID.Common.Interfaces;
+using EGID.Infrastructure.Auth;
+using EGID.Infrastructure.Crypto;
+using EGID.Infrastructure.DigitalSignature;
+using EGID.Infrastructure.KeysGenerator;
+using EGID.Infrastructure.Time;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace EGID.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient<IKeyGeneratorService, KeyGeneratorService>();
+            services.AddTransient<IAesCryptoService, AesCryptoService>();
+            services.AddTransient<IDigitalSignatureService, DigitalSignatureService>();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<AuthDbContext>(options => options.UseSqlServer(connectionString));
+
+            services.AddIdentity<CitizenAccount, IdentityRole>(options =>
+                {
+                    options.Password.RequiredLength = 8;
+                    options.Password.RequireLowercase = true;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireDigit = true;
+                    options.Lockout.DefaultLockoutTimeSpan = new TimeSpan(0, 0, 5, 0);
+                })
+                .AddEntityFrameworkStores<AuthDbContext>()
+                .AddDefaultTokenProviders();
+
+            // remove default claims
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(config =>
+                {
+                    config.RequireHttpsMetadata = true;
+                    config.SaveToken = true;
+                    config.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtKey"])),
+                        ValidIssuer = configuration["JwtIssuer"],
+                        ValidAudience = configuration["JwtIssuer"],
+                        ValidateIssuerSigningKey = true,
+                        ValidateLifetime = true,
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ClockSkew = TimeSpan.Zero
+                    };
+                });
+
+            services.AddTransient<IDateTime, UtcDateTime>();
+
+            return services;
+        }
+    }
+}
--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -1,21 +1,13 @@
-using System;
-using System.IdentityModel.Tokens.Jwt;
-using System.Text;
+using EGID.Application;
 using EGID.Data;
-using EGID.Data.Cards;
-using EGID.Infrastructure.Crypto;
-using EGID.Infrastructure.DigitalSignature;
-using EGID.Infrastructure.KeysGenerator;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using EGID.Infrastructure;
+using EGID.Web.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Routing;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
 
 namespace EGID.Web
 {
@@ -28,64 +20,21 @@ namespace EGID.Web
         // Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+            services.AddTransient<IFilesDirectoryService, FilesDirectoryService>();
+            services.AddScoped<ICurrentUserService, CurrentUserService>();
 
-            services.AddDbContext<EgidDbContext>(options => options.UseSqlServer(connectionString));
+            services.AddHttpContextAccessor();
 
-            services.AddIdentity<Card, IdentityRole>(options =>
-                {
-                    options.Password.RequiredLength = 8;
-                    options.Password.RequireLowercase = true;
-                    options.Password.RequireUppercase = false;
-                    options.Password.RequireDigit = true;
-                    options.Lockout.DefaultLockoutTimeSpan = new TimeSpan(0, 0, 5, 0);
-                })
-                .AddEntityFrameworkStores<EgidDbContext>()
-                .AddDefaultTokenProviders();
-
-            // remove default claims
-            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
-
-            services.AddAuthentication(options =>
-                {
-                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                })
-                .AddJwtBearer(config =>
-                {
-                    config.RequireHttpsMetadata = true;
-                    config.SaveToken = true;
-                    config.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JwtKey"])),
-                        ValidIssuer = Configuration["JwtIssuer"],
-                        ValidAudience = Configuration["JwtIssuer"],
-                        ValidateIssuerSigningKey = true,
-                        ValidateLifetime = true,
-                        ValidateIssuer = true,
-                        ValidateAudience = true,
-                        ClockSkew = TimeSpan.Zero
-                    };
-                });
+            services.AddInfrastructure(Configuration);
+            services.AddDatabase(Configuration);
 
             services.Configure<RouteOptions>(options =>
             {
                 options.LowercaseUrls = true;
                 options.AppendTrailingSlash = false;
             });
-
-            services.AddTransient<ICardService, CardService>();
-
-            // add app services
-            services.AddTransient<IKeyGeneratorService, KeyGeneratorService>();
-            services.AddTransient<IAesCryptoService, AesCryptoService>();
-            services.AddTransient<IDigitalSignatureService, DigitalSignatureService>();
-
-            services.AddControllers().AddJsonOptions(options =>
-            {
-                options.JsonSerializerOptions.IgnoreNullValues = true;
-            });
+            services.AddControllers()
+                .AddJsonOptions(options => { options.JsonSerializerOptions.IgnoreNullValues = true; });
         }
 
         // Use this method to configure the HTTP request pipeline.
