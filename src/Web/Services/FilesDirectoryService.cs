bb583c3384d98f730086349a1853cdf93279e989
Mahmoud Shaheen <mxshaheen@outlook.com>
2020-04-29 01:18:09 +0200

    BE | file dircetory service

---
+++ src/Web/Services/FilesDirectoryService.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using EGID.Application;
+using Microsoft.AspNetCore.Hosting;
+
+namespace EGID.Web.Services
+{
+    public class FilesDirectoryService : IFilesDirectoryService
+    {
+        public FilesDirectoryService(IWebHostEnvironment env)
+        {
+            CitizenPhotosDirectory = Path.Combine(env.WebRootPath, "Img", "CitizenPhotos");
+        }
+
+        public string CitizenPhotosDirectory { get; }
+    }
+
+    
+}
