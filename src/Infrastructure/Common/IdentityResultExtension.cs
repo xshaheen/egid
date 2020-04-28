335a1d7859b7cf253e87099791c6e4d4f48e0be0
Mahmoud Shaheen <mxshaheen@outlook.com>
2020-04-29 01:14:05 +0200

    BE | Identity resutl to result extension

---
+++ src/Infrastructure/Common/IdentityResultExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using EGID.Common.Models.Result;
+using Microsoft.AspNetCore.Identity;
+
+namespace EGID.Infrastructure.Common
+{
+    public static class IdentityResultExtension
+    {
+        public static Result ToResult(this IdentityResult identityResult)
+        {
+            return identityResult.Succeeded
+                ? Result.Success()
+                : Result.Failure(identityResult.Errors.Select(i => i.Description));
+        }
+    }
+}
