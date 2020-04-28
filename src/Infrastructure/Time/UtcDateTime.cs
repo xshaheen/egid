71e6170b513aeacf93e22885a102dd29de1fda84
Mahmoud Shaheen <mxshaheen@outlook.com>
2020-04-29 01:12:07 +0200

    BE | current date time service

---
+++ src/Infrastructure/Time/UtcDateTime.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using EGID.Common.Interfaces;
+
+namespace EGID.Infrastructure.Time
+{
+    public class UtcDateTime : IDateTime
+    {
+        public DateTime Now => DateTime.UtcNow;
+    }
+}
