b0d1d86361dcdeced00d6898d7bafc7aecd69f58
Mahmoud Shaheen <mxshaheen@outlook.com>
2020-04-29 04:13:59 +0200

    BE | auto create database or apply new migrations

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,13 +1,43 @@
+using System;
+using System.Threading.Tasks;
+using EGID.Data;
+using EGID.Infrastructure.Auth;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace EGID.Web
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scoped = host.Services.CreateScope())
+            {
+                var services = scoped.ServiceProvider;
+
+                try
+                {
+                    // Create database if not already exist or apply any new migration
+
+                    var appDbContext = services.GetService<EgidDbContext>();
+                    await appDbContext.Database.MigrateAsync();
+
+                    var authDbContext = services.GetService<AuthDbContext>();
+                    await authDbContext.Database.MigrateAsync();
+                }
+                catch (Exception e)
+                {
+                    var logger = services.GetService<ILogger<Program>>();
+                    logger.LogError(e, "An error occured while migrating or initializing the database.");
+                }
+            }
+
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
