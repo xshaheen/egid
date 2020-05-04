906f204fdd29b01aa7b7947d11906986971b8271
Mahmoud Shaheen <mxshaheen@outlook.com>
2020-05-04 08:12:35 +0200

    BE | authentication controller

--- src/Web/Controllers/AuthController.cs
+++ src/Web/Controllers/AuthController.cs
@@ -1,63 +1,22 @@
-﻿using System;
-using EGID.Application;
-using EGID.Infrastructure.Auth;
+﻿using System.Threading.Tasks;
+using EGID.Application.Cards.Login;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 
 namespace EGID.Web.Controllers
 {
-    public class AuthController : ControllerBase
+    [AllowAnonymous]
+    public class AuthController : BaseController
     {
-        private readonly IKeyGeneratorService _keyGenerator;
-        private readonly IConfiguration _configuration;
-
-        public AuthController(IKeyGeneratorService keyGenerator, IConfiguration configuration)
-        {
-            _keyGenerator = keyGenerator;
-            _configuration = configuration;
-        }
-
         [HttpPost]
-        public ActionResult SignIn()
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> SignIn([FromBody] LoginCommand command)
         {
-            // validate
-            // if (ModelState.IsValid)
-            //     return UnprocessableEntity(ModelState.Values);
+            var token = await Mediator.Send(command);
 
-            var card = new Card
-            {
-                Id = Guid.NewGuid().ToString(),
-                PublicKeyXml = _keyGenerator.PublicKeyXml,
-                PrivateKeyXml = _keyGenerator.PrivateKeyXml,
-                Puk = "123456",
-                Pin1 = "123456",
-                Pin2 = "123456",
-            };
-
-            // process
-
-            // if (result.Failed)
-            //     return BadRequest(result.Errors);
-
-            return Ok(/*GenerateJwtToken(card.Id)*/);
-        }
-
-        [HttpPost]
-        public ActionResult ChangePuk()
-        {
-            return Ok();
-        }
-
-        [HttpPost]
-        public ActionResult ChangePin1()
-        {
-            return Ok();
-        }
-
-        [HttpPost]
-        public ActionResult ChangePin2()
-        {
-            return Ok();
+            return Ok(token);
         }
     }
 }
