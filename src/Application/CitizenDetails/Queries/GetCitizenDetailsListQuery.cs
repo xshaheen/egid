ee09f0a6ce6c1823881d472ce05a3c4d63ebed30
Mahmoud Shaheen <mxshaheen@outlook.com>
2020-05-06 10:33:31 +0200

    BE | Citizen controller

---
+++ src/Application/CitizenDetails/Queries/GetCitizenDetailsListQuery.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using EGID.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace EGID.Application.CitizenDetails.Queries
+{
+    public class GetCitizenDetailsListQuery : IRequest<List<CitizenDetailsVm>>
+    {
+        #region Handler
+
+        public class GetCitizenDetailsQueryHandler : IRequestHandler<GetCitizenDetailsListQuery, List<CitizenDetailsVm>>
+        {
+            private readonly IEgidDbContext _context;
+
+            public GetCitizenDetailsQueryHandler(IEgidDbContext context) => _context = context;
+
+            public async Task<List<CitizenDetailsVm>> Handle(
+                GetCitizenDetailsListQuery request, CancellationToken cancellationToken
+            )
+            {
+                var citizen = await _context.CitizenDetails
+                    .Select(c => new CitizenDetailsVm
+                    {
+                        Id = c.Id,
+                        FatherId = c.FatherId,
+                        MotherId = c.MotherId,
+                        FullName = c.FullName,
+                        Address = c.Address,
+                        Gender = c.Gender,
+                        Religion = c.Religion,
+                        SocialStatus = c.SocialStatus,
+                        DateOfBirth = c.DateOfBirth,
+                        PhotoUrl = c.PhotoUrl,
+                        Create = c.Create,
+                        CreateBy = c.CreateBy,
+                        LastModified = c.LastModified,
+                        LastModifiedBy = c.LastModifiedBy
+                    })
+                    .OrderBy(c => c.Create)
+                    .ToListAsync(cancellationToken);
+
+                return citizen;
+            }
+        }
+
+        #endregion
+    }
+}
--- src/Web/Controllers/CitizensController.cs
+++ src/Web/Controllers/CitizensController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using EGID.Application.CitizenDetails.Commands;
+using EGID.Application.CitizenDetails.Queries;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,12 +8,56 @@ namespace EGID.Web.Controllers
 {
     public class CitizensController : BaseController
     {
-        [HttpPost]
+        [HttpGet("/")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult> Get()
+        {
+            var citizens= await Mediator.Send(new GetCitizenDetailsListQuery());
+
+            return Ok(citizens);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> Get(string id, [FromBody] GetCitizenDetailsQuery query)
+        {
+            if (id != query.Id) return NotFound();
+
+            await Mediator.Send(query);
+
+            return NoContent();
+        }
+
+        [HttpPost("/")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesDefaultResponseType]
         public async Task<ActionResult> Post([FromBody] CreateCitizenCommand command)
         {
             await Mediator.Send(command);
+
+            return NoContent();
+        }
+
+        [HttpPost("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> Update(string id,[FromBody] UpdateCitizenCommand command)
+        {
+            if (id != command.Id) return NotFound();
+
+            await Mediator.Send(command);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> Delete(string id)
+        {
+            await Mediator.Send(new DeleteCitizenCommand { Id = id });
 
             return NoContent();
         }
