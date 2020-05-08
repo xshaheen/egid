d0bed2cc29d10bcc0bb0af23d5355873c05db7d7
Mahmoud Shaheen <mxshaheen@outlook.com>
2020-05-08 05:57:12 +0200

    FE | authentication service and remove unnecessary models

--- src/Web/ClientApp/src/app/services/auth.service.ts
+++ src/Web/ClientApp/src/app/services/auth.service.ts
@@ -1,33 +1,31 @@
-import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
 import { AppTokenService } from "./token.service";
-
-export class SignInModel {
-  privateKey!: string;
-  pin1!: string;
-}
+import { AuthClient, LoginCommand } from "../api";
 
 @Injectable({ providedIn: "root" })
 export class AuthService {
   constructor(
-    private readonly http: HttpClient,
+    private readonly authClient: AuthClient,
     private readonly router: Router,
     private readonly appTokenService: AppTokenService
   ) {}
 
-  signIn(model: SignInModel): void {
-    this.http.post<string>("/api/auth", model).subscribe((token) => {
+  public get isAuthenticated(): boolean {
+    return this.appTokenService.any();
+  }
+
+  signIn(model: LoginCommand): void {
+    this.authClient.signIn(model).subscribe((token) => {
       if (!token) {
         return;
       }
       this.appTokenService.set(token);
-      this.router.navigate(["/main/home"]);
     });
   }
 
   signOut() {
     this.appTokenService.clear();
-    this.router.navigate(["/auth"]);
+    this.router.navigate(["/main/home"]);
   }
 }
