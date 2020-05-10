ee2b1d4093d22e50feff4c3a567347bd6dfba32b
Mahmoud Shaheen <mxshaheen@outlook.com>
2020-05-10 10:54:10 +0200

    FE | forbidden component

---
+++ src/Web/ClientApp/src/app/views/forbidden/forbidden.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  templateUrl: "forbidden.component.html",
+  styleUrls: ["forbidden.component.scss"],
+})
+export class ForbiddenComponent {}
