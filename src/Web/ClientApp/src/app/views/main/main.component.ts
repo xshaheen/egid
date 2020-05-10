29d7d1db5e67be6e9241b3f72174a82947b6672e
Mahmoud Shaheen <mxshaheen@outlook.com>
2020-05-10 15:22:53 +0200

    FE | fix login button in main page

--- src/Web/ClientApp/src/app/views/main/main.component.ts
+++ src/Web/ClientApp/src/app/views/main/main.component.ts
@@ -1,22 +1,14 @@
 import { Component } from "@angular/core";
-import { MatDialog } from "@angular/material/dialog";
-import { LoginDialogComponent } from "src/app/components/login-dialog/login-dialog.component";
 import { AuthService } from "src/app/services/auth.service";
+import { LoginService } from "src/app/services/login.service";
 
 @Component({
-  selector: "eg-main",
   templateUrl: "./main.component.html",
   styleUrls: ["./main.component.scss"],
 })
 export class MainComponent {
-  constructor(private dialog: MatDialog, public authService: AuthService) {}
-
-  openLoginDialog(): void {
-    this.dialog.open(LoginDialogComponent, {
-      width: "550px",
-      direction: "rtl",
-      disableClose: true,
-      closeOnNavigation: true,
-    });
-  }
+  constructor(
+    public authService: AuthService,
+    public loginService: LoginService
+  ) {}
 }
