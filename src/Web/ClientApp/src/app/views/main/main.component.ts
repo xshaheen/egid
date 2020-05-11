d16923bd651bf6c9931d8c0c9353d8ac71d8a47d
Mahmoud Shaheen <mxshaheen@outlook.com>
2020-05-11 08:09:02 +0200

    FE | main page

--- src/Web/ClientApp/src/app/views/main/main.component.ts
+++ src/Web/ClientApp/src/app/views/main/main.component.ts
@@ -1,14 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { AuthService } from "src/app/services/auth.service";
 import { LoginService } from "src/app/services/login.service";
+import { BreakpointsService } from "src/app/services/breakpoints.service";
 
 @Component({
   templateUrl: "./main.component.html",
   styleUrls: ["./main.component.scss"],
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+  isHandset: boolean;
+
   constructor(
+    private breakpoints: BreakpointsService,
     public authService: AuthService,
     public loginService: LoginService
   ) {}
+
+  ngOnInit(): void {
+    this.breakpoints
+      .isHandset()
+      .subscribe((result) => (this.isHandset = result));
+  }
 }
