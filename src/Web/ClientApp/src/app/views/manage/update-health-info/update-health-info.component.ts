09a510108cef8fcf081aef64bee5c13a7100b2db
Mahmoud Shaheen <mxshaheen@outlook.com>
2020-05-11 18:23:29 +0200

    FE | update health info component

--- src/Web/ClientApp/src/app/views/manage/update-health-info/update-health-info.component.ts
+++ src/Web/ClientApp/src/app/views/manage/update-health-info/update-health-info.component.ts
@@ -1,12 +1,62 @@
 import { Component, OnInit } from "@angular/core";
+import {
+  UpdateEmergencyPhonesCommand,
+  IHealthInfoVm,
+  HealthInfoClient,
+  IEmergencyInfo,
+} from "src/app/api";
+import { AuthService } from "src/app/services/auth.service";
+import { ErrorService } from "src/app/services/error.service";
+import { NgForm } from "@angular/forms";
+import { AppModalService } from "src/app/services/modal.service";
 
 @Component({
-  selector: "eg-update-health-info",
   templateUrl: "./update-health-info.component.html",
-  styleUrls: ["./update-health-info.component.scss"]
+  styleUrls: ["./update-health-info.component.scss"],
 })
 export class UpdateHealthInfoComponent implements OnInit {
-  constructor() {}
+  command = new UpdateEmergencyPhonesCommand();
 
-  ngOnInit(): void {}
+  constructor(
+    private client: HealthInfoClient,
+    private authService: AuthService,
+    private errorHandler: ErrorService,
+    private modal: AppModalService
+  ) {}
+
+  ngOnInit(): void {
+    const healthId = this.authService.healthId;
+
+    this.client.emergencyInfo(healthId).subscribe(
+      (result) => {
+        this.command.healthInfoId = healthId;
+        this.command.phone1 = result.phone1;
+        this.command.phone2 = result.phone2;
+        this.command.phone3 = result.phone3;
+        this.command.notes = result.notes;
+      },
+      (err) => this.errorHandler.handleError(err)
+    );
+  }
+
+  Submit(form: NgForm) {
+    this.client.emergencyPhones(this.command).subscribe(
+      () => {
+        this.modal.showSuccessSnackBar("✔ تم حفظ التغيرات.");
+      },
+      (err) => {
+        const errors = JSON.parse(err.response).errors;
+
+        if (errors && errors.Phone1) {
+          this.modal.showErrorSnackBar("الرقم الاول: " + errors.Phone1[0]);
+        } else if (errors && errors.Phone2) {
+          this.modal.showErrorSnackBar("الرقم الثاني: " + errors.Phone1[0]);
+        } else if (errors && errors.Phone3) {
+          this.modal.showErrorSnackBar("الرقم الثالث: " + errors.Phone1[0]);
+        } else if (errors && errors.Note) {
+          this.modal.showErrorSnackBar("الملاحظات: " + errors.Phone1[0]);
+        }
+      }
+    );
+  }
 }
