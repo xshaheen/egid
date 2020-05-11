6d8ced6cf70d5941b8ddfa862fee6c8423cbb8ef
Mahmoud Shaheen <mxshaheen@outlook.com>
2020-05-11 04:11:11 +0200

    FE | remove unnecessary component

--- src/Web/ClientApp/src/app/views/admin/admin.module.ts
+++ src/Web/ClientApp/src/app/views/admin/admin.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { AdminRoutingModule } from "./admin-routing.module";
-import { QrWriterModule } from "src/app/components/qr-writer/qr-writer.module";
 import { MaterialModule } from "src/app/material.module";
 
 import { AdminComponent } from "./admin.component";
 import { EmployeesComponent } from "./employees/employees.component";
-import { RegisterEmployeeComponent } from "./register-employee/register-employee.component";
 
 @NgModule({
-  declarations: [AdminComponent, RegisterEmployeeComponent, EmployeesComponent],
-  imports: [CommonModule, MaterialModule, AdminRoutingModule, QrWriterModule],
+  declarations: [AdminComponent, EmployeesComponent],
+  imports: [CommonModule, MaterialModule, AdminRoutingModule],
 })
 export class AdminModule {}
