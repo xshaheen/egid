67c3de82b1b25c4686c9c27a5b2b33737a404a93
Mahmoud Shaheen <mxshaheen@outlook.com>
2020-05-11 15:16:34 +0200

    FE | improve error handling

--- src/Web/ClientApp/src/app/core/error.handler.ts
+++ src/Web/ClientApp/src/app/core/error.handler.ts
@@ -1,10 +1,12 @@
 import { ErrorHandler, Injectable, Injector } from "@angular/core";
+import { AppModalService } from "../services/modal.service";
 
 @Injectable({ providedIn: "root" })
 export class AppErrorHandler implements ErrorHandler {
+  constructor(private modal: AppModalService) {}
   handleError(error: any) {
     // ... log to logging server
-
-    console.error("#ERROR#", error);
+    this.modal.showErrorSnackBar("حدث خطأ اثناء تنفيذ طلبك.");
+    console.error("#AppErrorHandler detect an error:", error);
   }
 }
--- src/Web/ClientApp/src/app/services/error.service.ts
+++ src/Web/ClientApp/src/app/services/error.service.ts
@@ -7,10 +7,7 @@ export class ErrorService {
   constructor(private readonly modal: AppModalService) {}
 
   handleError(error: any): void {
-    if (error instanceof ProblemDetails) {
-      this.modal.showErrorSnackBar(error.detail);
-      return;
-    } else if (error instanceof ApiException) {
+    if (error instanceof ApiException) {
       switch (error.status) {
         case 404: {
           console.log("#ERROR 404#", error);
@@ -33,6 +30,17 @@ export class ErrorService {
           return;
         }
       }
+    } else if (error instanceof ProblemDetails) {
+      console.log("#Problem details");
+      console.log(error);
+
+      if (!error || !error.detail || error.status === 401) {
+        // is handled by the interceptor
+        return;
+      }
+
+      this.modal.showErrorSnackBar(error.detail);
+      return;
     }
 
     console.error("#ERROR#", error);
