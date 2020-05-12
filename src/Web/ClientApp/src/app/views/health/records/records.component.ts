c3b01c7fb469f5b0755a41d6fa62f1d29ec8aec2
Mahmoud Shaheen <mxshaheen@outlook.com>
2020-05-12 10:05:07 +0200

    FE | records component style

--- src/Web/ClientApp/src/app/views/health/records/records.component.ts
+++ src/Web/ClientApp/src/app/views/health/records/records.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from "@angular/core";
+import { BreakpointsService } from "src/app/services/breakpoints.service";
 
 @Component({
   selector: "eg-records",
   templateUrl: "./records.component.html",
-  styleUrls: ["./records.component.scss"]
+  styleUrls: ["./records.component.scss"],
 })
 export class RecordsComponent implements OnInit {
-  constructor() {}
+  isHandset: boolean;
 
-  ngOnInit(): void {}
+  constructor(private handset: BreakpointsService) {}
+
+  ngOnInit(): void {
+    this.handset.isHandset().subscribe((result) => (this.isHandset = result));
+  }
 }
